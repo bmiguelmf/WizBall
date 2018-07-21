d65e8e2a8ee71a9afbac8bd110d4452e18d88682
joaosantosatec <joao.santos.t0112400@edu.atec.pt>
2018-07-21 23:05:04 +0100

    Evolução Sistema Login

---
+++ 01 - WizBall Solution/WebApp/App_Code/GLOBALS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.App_Code
+{
+    public class GLOBALS
+    {
+        public static string GetTeamImg(string area, string team)
+        {
+
+            return string.Format("..\\resources\\teams\\{0}\\{1}.ico", area, team);
+        }
+
+        public static string GetFedImg(string area)
+        {
+            return string.Format("..\\federations\\{0}.ico", area);
+        }
+    }
+}
---
+++ 01 - WizBall Solution/WebApp/datadeploy.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BusinessLogic.BLL;
+
+namespace WebApp
+{
+    public partial class datadeploy : System.Web.UI.Page
+    {
+        BLL bll = new BLL();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void InAreas_Click(object sender, EventArgs e)
+        {
+            bll.FullDatabaseSync();
+        }
+
+        protected void InSeasons_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void InComps_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void InTeams_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
