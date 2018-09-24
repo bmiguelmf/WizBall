b18ad9ffb3323e55fcdc15940ef265956f505a46
joaosantosatec <T0112400@ciber.local>
2018-09-24 23:10:11 +0100

    TipsHistory

---
+++ 01 - WizBall Solution/FrontOffice/Pages/ViewHistory.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.HtmlControls;
+using BusinessLogic.Entities;
+using System.Web.UI.WebControls;
+using BusinessLogic.BLL;
+using System.Web.Configuration;
+using FrontOffice.Resources;
+
+namespace FrontOffice.Pages
+{
+    public partial class ViewHistory : System.Web.UI.Page
+    {
+        private static BLL bll;
+        private static string apiToken;
+        private static string connString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsPostBack) return;
+
+
+            apiToken = WebConfigurationManager.AppSettings["ApiToken"];
+            connString = WebConfigurationManager.ConnectionStrings["home"].ConnectionString;
+            bll = new BLL(connString, apiToken);
+
+
+
+
+            MatchesTipsGrid matchesTipsGrid = new MatchesTipsGrid(bll.GetMatchesByRangeDateAndCompetition());
+
+
+            placeHolderHistoryTips.Controls.Add(matchesTipsGrid.Create());
+        }
+    }
+}
