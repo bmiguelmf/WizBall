250798287393fb5b8bc1396b8bcc0cda1e58edec
bmiguelmf <bmiguelmf@gmail.com>
2018-10-04 15:18:25 +0100

    implementado filtro por competição na view home

--- 01 - WizBall Solution/FrontOffice/Public/Javascript/MatchesTipsGrid.js
+++ 01 - WizBall Solution/FrontOffice/Public/Javascript/MatchesTipsGrid.js
@@ -3,6 +3,7 @@ window.addEventListener("resize", setMatchesTipsGridHeight);
 
 var scrollableHeigth = $("#grid-body-scrollable").height();
 
+
 function setMatchesTipsGridHeight() {
 
     // windows height.
@@ -11,8 +12,11 @@ function setMatchesTipsGridHeight() {
     // Navigation height.
     var navHeight = $("#app-header").height();
 
+    // Navigation height.
+    var compFilters = $("#filters").outerHeight();
+
     // Free height.
-    var freeHeight = winHeight - navHeight -30 -100;
+    var freeHeight = winHeight - navHeight - compFilters -30 -100;
 
 
     while (freeHeight % 30 !== 0) {
@@ -75,3 +79,83 @@ function convertUtcDateToLocalDate() {
 // Exec one time functions.
 setMatchesTipsGridHeight();
 convertUtcDateToLocalDate();
+
+
+
+
+
+//**********************************************************************
+// Competitions filters
+//**********************************************************************
+document.getElementById("filters").addEventListener("click", function (e) {
+
+    if (e.target && (e.target.id === "show-all-outter" || e.target.id === "show-all")) {
+
+        alert("All");
+     
+    }
+    else if (e.target && (e.target.id === "show-none-outter" || e.target.id === "show-none")) {
+
+        alert("none");
+
+    }
+    else if (e.target && e.target.nodeName === "DIV") {
+
+        var div = document.getElementById(e.target.id);
+
+        if (div.classList.contains("competition")) {
+
+            div.classList.toggle("unselected");
+
+            filterByCompetitions();
+        }
+     
+    }   
+    else if (e.target && e.target.nodeName === "IMG") {
+
+        document.getElementById(e.target.id).parentElement.classList.toggle("unselected");
+
+        filterByCompetitions();
+    }
+
+});
+
+function filterByCompetitions() {
+
+    var children = document.getElementById("filters").childNodes;       // Gets div where imgs are.                          
+
+    for (var i = 0; i < children.length; i++) {                         // For each div lets check if it has the unselected class.
+
+        compId = children[i].getAttribute("id");                        // Gets the competition id which will be needed either way.
+
+        if (children[i].classList.contains("unselected")) {        
+            hideFilter(compId);                                         // Hides all rows which belongs to the respective competition id
+        }
+        else {
+            showFilter(compId);                                        // Show all rows which belongs to the respective competition id
+        }
+    }
+}
+
+function showFilter(competitionId) {
+
+    var gridRows = document.getElementById("grid-body").childNodes;
+
+    for (var i = 0; i < gridRows.length; i++) {
+
+        if (gridRows[i].getAttribute("compid") === competitionId) {
+            gridRows[i].style.display = "flex";
+        }
+    }
+}
+function hideFilter(competitionId) {
+
+    var gridRows = document.getElementById("grid-body").childNodes;
+
+    for (var i = 0; i < gridRows.length; i++) {
+
+        if (gridRows[i].getAttribute("compid") === competitionId) {
+            gridRows[i].style.display = "none";
+        }
+    }
+}
