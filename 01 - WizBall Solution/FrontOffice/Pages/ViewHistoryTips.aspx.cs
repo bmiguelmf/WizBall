7da511280b78a50eebfaeae133e3d91981a338a1
Bruno Ferreira <bruno.ferreira.t0110407@edu.atec.pt>
2018-10-18 20:44:31 +0100

    fds

--- 01 - WizBall Solution/FrontOffice/Pages/ViewHistoryTips.aspx.cs
+++ 01 - WizBall Solution/FrontOffice/Pages/ViewHistoryTips.aspx.cs
@@ -5,17 +5,31 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using BusinessLogic.Entities;
 
 namespace FrontOffice.Pages
 {
     public partial class ViewHistoryTips : System.Web.UI.Page
     {
+        private bool IsUserLoggedIn()
+        {
+            User user = Session["User"] as User;
+
+            return user is null ? false : true;
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
-            MatchesTipsHistoryGrid matchesTipsHistoryGrid = new MatchesTipsHistoryGrid();
-
-            placeHolderTipsHistoryGrid.Controls.Add(matchesTipsHistoryGrid.FiltersAndGrid());
+            if (IsUserLoggedIn() &&  (Session["User"] as User).CurrentUserHistory.AfterState.Id == 21)
+            {
+                MatchesTipsHistoryGrid matchesTipsHistoryGrid = new MatchesTipsHistoryGrid();
+                placeHolderTipsHistoryGrid.Controls.Add(matchesTipsHistoryGrid.FiltersAndGrid());
+            }
+            else
+            {
+                Page.Response.Write("Exclusivo");
+            }
+            
         }
     }
 }
