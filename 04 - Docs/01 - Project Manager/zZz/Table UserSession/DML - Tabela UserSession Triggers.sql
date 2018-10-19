8d231bbd086ed823fb2799fd12c56407807fbb0f
bmiguelmf <bmiguelmf@gmail.com>
2018-10-19 14:59:57 +0100

    sql

---
+++ 04 - Docs/01 - Project Manager/zZz/Table UserSession/DML - Tabela UserSession Triggers.sql
@@ -0,0 +1,65 @@
+-------------------------------------------------------------------------------------
+-- Trabalho Prático de Avaliação da UFCD 5411 – Base de Dados – Sistemas de Gestão --
+-- Módulo User_Profile															   --
+-- Tabela UserSession 												   	  		           --
+-- Bruno Ferreira 19/10/2018													   --
+-------------------------------------------------------------------------------------
+
+
+-- preset databse
+use TrabalhoSQL
+go
+
+create or alter trigger trigge_userSessionLogs on userSession  after insert, delete, update
+as begin
+	declare @tableName		varchar(25)			= 'userSession ';
+	declare @actionType		varchar(10);
+	declare @contents		varchar(max);
+	declare @userId			int;
+	
+
+
+	-- delete
+	if not exists(select * from inserted)
+		
+		begin
+
+			set @actionType = 'Delete';
+			set @contents	= 'Delete do registo de userSession  nº ' + convert(varchar(10), (select userSessionId from deleted));
+			
+			set @userId = (select top 1 userId from [user] where username = (select createdBy from deleted));
+
+			exec insertLog @tableName, @actionType, @contents, @userId
+
+		end
+
+
+	-- insert
+	else if not exists(select * from deleted)
+
+		begin
+
+			set @actionType = 'Insert';
+			set @contents	= 'Insert do registo de userSession  nº ' + convert(varchar(10), (select userSessionId from inserted));
+
+			set @userId = (select top 1 userId from [user] where username = (select createdBy from inserted));
+
+			exec insertLog @tableName, @actionType, @contents, @userId
+
+		end
+
+
+	-- update
+	else
+
+		begin
+			
+			set @actionType = 'Update';
+			set @contents	= 'Update do registo de userSession  nº ' + convert(varchar(10), (select userSessionId from inserted));
+
+			set @userId = (select top 1 userId from [user] where username = (select createdBy from deleted));
+
+			exec insertLog @tableName, @actionType, @contents, @userId
+
+		end
+end
